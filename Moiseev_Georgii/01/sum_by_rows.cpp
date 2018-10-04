1701549a3cf5eb667ef6d2668634329e8e287520
Георгий <moiseev.georgii@gmail.com>
2018-10-04 22:24:39 +0300

    expanded test matrix size

--- Moiseev_Georgii/01/sum_by_rows.cpp
+++ Moiseev_Georgii/01/sum_by_rows.cpp
@@ -1,19 +1,28 @@
+#include <cstdint>
 #include "timer.h"
 
 int main(void)
 {
-	size_t row_size = 1000;
-	size_t col_size = 1000;
+	size_t row_size = 10000;
+	size_t col_size = 10000;
 
-	int array[col_size][row_size];
+	int32_t** array = new int32_t*[col_size];
+	for (size_t col = 0; col < col_size; col++)
+		array[col] = new int32_t[row_size];
 
 	{
+		volatile int32_t sum = 0;
 		Timer T;
-		volatile int sum = 0;
+		
 		for (size_t col = 0; col < col_size; col++)
 			for (size_t row = 0; row < row_size; row++)
 				sum += array[col][row];
 	}
+
+
+	for (size_t col = 0; col < col_size; col++)
+		delete[] array[col];
+	delete[] array;
 
 	return 0;
 }
