1f4caece8d07c4ff49268c448ecc5bc40e0a173c
KozlovAlexandr <kostebo98@mail.ru>
2018-10-10 16:02:37 +0300

    Update sum_by_rows.cpp

--- Kozlov/01/sum_by_rows.cpp
+++ Kozlov/01/sum_by_rows.cpp
@@ -1,23 +1,22 @@
 #include <chrono>
 #include <iostream>
 
-class Timer
-{
-    public:
+class Timer {
 
-        Timer()
-            : start_(std::chrono::high_resolution_clock::now())
-        {
-        }
+public:
+    Timer() : start_(std::chrono::high_resolution_clock::now())
+    {
+    }
 
-        ~Timer()
-        {
-            const auto finish = std::chrono::high_resolution_clock::now();
-            std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count() << " us" << std::endl;
-        }
+    ~Timer()
+    {
+        const auto finish = std::chrono::high_resolution_clock::now();
+        std::cout << std::chrono::duration_cast<std::chrono::microseconds>(finish - start_).count()
+            << " us" << std::endl;
+    }
 
-    private:
-        const std::chrono::high_resolution_clock::time_point start_;
+private:
+    const std::chrono::high_resolution_clock::time_point start_;
 };
 
 int main()
@@ -25,19 +24,24 @@ int main()
     const size_t size = 10000;
     volatile int s;
     int **a;
-    a = (int**)malloc(sizeof(int*)*size);
-    for (size_t i = 0; i < size; ++i)
-        a[i] = (int*)malloc(sizeof(int)*size);
 
-    {
-    Timer t;
-    for (size_t i = 0; i < size; ++i)
-        for (size_t j = 0; j < size; ++j)
-            s += a[i][j];
+    a = (int**)malloc(sizeof(int*)*size);
+    for (size_t i = 0; i < size; ++i) {
+        a[i] = (int*)malloc(sizeof(int)*size);
     }
 
-    for (size_t i = 0; i < size; ++i)
+    {
+        Timer t;
+        for (size_t i = 0; i < size; ++i) {
+            for (size_t j = 0; j < size; ++j) {
+                s += a[i][j];
+            }
+        }
+    }
+
+    for (size_t i = 0; i < size; ++i) {
         free(a[i]);
+    }
     free(a);
 
     return 0;
