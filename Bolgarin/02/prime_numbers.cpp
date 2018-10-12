463167e86647c13203ed69589c6c1f202b211bc6
maxbolgarin <maxbolgarin@yandex.ru>
2018-10-12 14:02:34 +0300

    Fix HW2

--- Bolgarin/02/prime_numbers.cpp
+++ Bolgarin/02/prime_numbers.cpp
@@ -2,17 +2,6 @@
 #include <memory>
 #include "numbers.dat"
 
-
-/*void fill(auto& array, size_t size, auto fill) {
-	for (size_t i = 0; i < size; i++)
-		array[i] = fill;
-}*/
-
-/*void fillTrue(std::unique_ptr<bool[]>& array, size_t size) {
-	for (size_t i = 0; i < size; i++)
-		array[i] = true;
-}*/
-
 auto getPrimeIndicator(size_t maxNumber) {
 
 	auto primeIndicator = std::make_unique<bool[]>(maxNumber + 1);
@@ -47,62 +36,62 @@ int main(int argc, char* argv[])
 
 	auto primeIndicator = getPrimeIndicator(Size);
 
-    for (int k = 1; k < nOfPairs*2; k+=2)
-    {
-        int start = std::atoi(argv[k]);
-        int finish = std::atoi(argv[k+1]);
+	for (int k = 1; k < nOfPairs*2; k+=2)
+	{
+		int start = std::atoi(argv[k]);
+		int finish = std::atoi(argv[k+1]);
 
-        if (start > finish) {
-        	std::cout << 0;
-        	return 0;
-        }
-
-
-
-        size_t indexMin = -1;
-        size_t indexMax = Size;
-
-        for (size_t i = 0; i < Size; i++) {
-        	if (Data[i] == start) {
-        		indexMin = i;
-        		break;
-        	}
-        }
-
-        if (indexMin == -1) {
-        	std::cout << 0;
-        	return 0;
-        }
-
-        for (size_t i = indexMin; i < Size; i++) {
-        	if (Data[i] == finish) {
-        		if (i+1 >= Size) {
-        			indexMax = i;
-        			break;
-        		} else if (Data[i+1] == finish) {
-        			continue;
-        		}
-        		indexMax = i;
-        		break;
-        	}
-        }
-
-        if (indexMax == Size) {
-        	std::cout << 0;
-        	return 0;
-        }
+		if (start > finish) {
+			std::cout << 0;
+			return 0;
+		}
 
 
 
-        int primeCounter = 0;
+		size_t indexMin = -1;
+		size_t indexMax = Size;
 
-        for (size_t i = indexMin; i <= indexMax; i++) {
-        	if (primeIndicator[Data[i]]) 
-        		primeCounter++;
-        }
+		for (size_t i = 0; i < Size; i++) {
+			if (Data[i] == start) {
+				indexMin = i;
+				break;
+			}
+		}
 
-        std::cout << primeCounter << std::endl;
-    }
+		if (indexMin == -1) {
+			std::cout << 0;
+			return 0;
+		}
+
+		for (size_t i = indexMin; i < Size; i++) {
+			if (Data[i] == finish) {
+				if (i+1 >= Size) {
+					indexMax = i;
+					break;
+				} else if (Data[i+1] == finish) {
+					continue;
+				}
+				indexMax = i;
+				break;
+			}
+		}
+
+		if (indexMax == Size) {
+			std::cout << 0;
+			return 0;
+		}
+
+
+
+		int primeCounter = 0;
+
+		for (size_t i = indexMin; i <= indexMax; i++)
+			if (primeIndicator[Data[i]]) 
+				primeCounter++;
+
+
+		std::cout << primeCounter << std::endl;
+	}
 
     return 0;
 }
