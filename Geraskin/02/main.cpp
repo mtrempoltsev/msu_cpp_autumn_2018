0cf3673767b80abc54a9f384974a4c075e389052
r1ckya <geraskin.ia@yandex.ru>
2018-10-13 10:51:52 +0300

    function split

--- Geraskin/02/main.cpp
+++ Geraskin/02/main.cpp
@@ -17,37 +17,43 @@ bool prime(const int n) {
     return true;
 }
 
-int main(int argc, char* argv[]) {
-    using std::cout;
-
-    std::vector<int> dp(Size + 1);
-
+void precalc(std::vector<int>& dp) {
     for (int i = 0; i < Size; ++i) {
         dp[i + 1] = dp[i] + prime(Data[i]);
     }
+}
+
+int calc_ans(const std::vector<int>& dp, int left, int right) {
+    if (left > right) {
+        return 0;
+    } else {
+        const auto first = std::lower_bound(Data, Data + Size, left);
+        const auto last = std::upper_bound(Data, Data + Size, right);
+
+        if (first == Data + Size || *first != left) {
+            return 0;
+        } else if (last == Data || *(last - 1) != right) {
+            return 0;
+        } else {
+            return dp[last - Data] - dp[first - Data];
+        }
+    }
+}
+
+int main(int argc, char* argv[]) {
+    using std::cout;
 
     if (!(argc & 1) || argc == 1) {
         return -1;
     }
 
+    std::vector<int> dp(Size + 1);
+    precalc(dp);
+
     for (int i = 1; i < argc; i += 2) {
         int left = std::atoi(argv[i]);
         int right = std::atoi(argv[i + 1]);
 
-        if (left > right) {
-            cout << 0;
-        } else {
-            const auto first = std::lower_bound(Data, Data + Size, left);
-            const auto last = std::upper_bound(Data, Data + Size, right);
-
-            if (first == Data + Size || *first != left) {
-                cout << 0;
-            } else if (last == Data || *(last - 1) != right) {
-                cout << 0;
-            } else {
-                cout << dp[last - Data] - dp[first - Data];
-            }
-        }
-        cout << '\n';
+        cout << calc_ans(dp, left, right) << '\n';
     }
 }
