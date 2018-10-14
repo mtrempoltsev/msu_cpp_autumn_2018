afee8813d0c947d34318ca6c86640554350f03ee
artem <arteeemik@gmail.com>
2018-10-14 23:02:29 +0300

    HW1 (function)

--- Artem_Baryshev/01/main.cpp
+++ Artem_Baryshev/01/main.cpp
@@ -3,15 +3,13 @@
 #include <stdio.h>
 #include <algorithm>
 #include <cmath>
+#include <stdlib.h>
 
 const int MAX_N = 1e5 + 1;
 
-int main(int argc, const char * argv[]) {
-    if (argc % 2 != 1 || argc == 1) {
-        return -1;
-    }
-    
-    int prime_numbers[MAX_N] = {0, };
+void
+counting_prime_numbers(int * prime_numbers)
+{
     prime_numbers[1] = 1;
     prime_numbers[0] = 1;
     for (int i = 2; i * i < MAX_N; ++i) {
@@ -21,8 +19,11 @@ int main(int argc, const char * argv[]) {
             }
         }
     }
-    
-    int prefix_ans[Size + 1] = {0, };
+}
+
+void
+counting_prefix_ans(int * prefix_ans, int * prime_numbers)
+{
     for (int i = 0; i < Size; ++i) {
         if (i == 0 && !prime_numbers[Data[i]]) {
             prefix_ans[i] = 1;
@@ -32,6 +33,32 @@ int main(int argc, const char * argv[]) {
             prefix_ans[i] = prefix_ans[i - 1];
         }
     }
+}
+
+int
+count_prime_numbers(int position_first, int position_second, int * prefix_ans)
+{
+    int ans = 0;
+    if (position_first <= position_second) {
+        if (position_first > 0) {
+            ans = prefix_ans[position_second] - prefix_ans[position_first - 1];
+        } else {
+            ans = prefix_ans[position_second];
+        }
+    }
+    return ans;
+}
+
+int main(int argc, const char * argv[]) {
+    if (argc % 2 != 1 || argc == 1) {
+        return -1;
+    }
+    
+    int *prime_numbers = new int[MAX_N];
+    counting_prime_numbers(prime_numbers);
+    
+    int *prefix_ans = new int[Size + 1];
+    counting_prefix_ans(prefix_ans, prime_numbers);
     
     for (int i = 1; i < argc; i += 2) {
         int first_value = std::atoi(argv[i]);
@@ -42,15 +69,10 @@ int main(int argc, const char * argv[]) {
             printf("%d\n", 0);
             continue;
         }
-        int ans = 0;
-        if (position_first <= position_second) {
-            if (position_first > 0) {
-                ans = prefix_ans[position_second] - prefix_ans[position_first - 1];
-            } else {
-                ans = prefix_ans[position_second];
-            }
-        }
+        int ans = count_prime_numbers(position_first, position_second, prefix_ans);
         printf("%d\n", ans);
     }
+    delete[] prime_numbers;
+    delete[] prefix_ans;
     return 0;
 }
