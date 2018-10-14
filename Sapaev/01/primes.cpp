7852b31c4ef4a149ffbfdf76b4cc0ff2b89d0a19
Dilshod Sapaev <dilshod070@MacBook-Pro-Dilshod.local>
2018-10-15 00:13:32 +0300

    HW1 Done 2nd time

--- Sapaev/01/primes.cpp
+++ Sapaev/01/primes.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <string>
+#include <exception>
 
 #include "numbers.dat"
 
@@ -8,25 +9,25 @@
 // Size - array size
 // Data in [0, 10000], typeof(data[0]) == int
 
-int count(int, int);
+int count(int, int, const int*);
 bool isPrime(int);
-int findl(int);
-int findr(int);
+int findl(int, const int*);
+int findr(int, const int*);
 
 int main(int argc, char* argv[])
 {
     try {
         if (argc % 2 == 0 || argc == 1) {
-            throw "Usage: {start, end}";
+            throw std::invalid_argument("Usage: {begin end}");
         }
         
         for (int i = 1; i < argc; i+=2) {
-            int v = count(std::atoi(argv[i]), std::atoi(argv[i + 1]));
+            int v = count(std::atoi(argv[i]), std::atoi(argv[i + 1]), Data);
             std::cout << v << std::endl;
         }
     }
-    catch (const char* str) {
-        std::cout << "Error:\n\t" << str << std::endl;
+    catch (const std::invalid_argument& except) {
+        std::cout << "Error:\n\t" << except.what() << std::endl;
         return -1;
     }
     return 0;
@@ -34,10 +35,10 @@ int main(int argc, char* argv[])
 
 // counts prime numbers in Data
 // searck between first "begin" value to last "end" value
-int count(int begin, int end)
+int count(int begin, int end, const int* Data)
 {
     int counter = 0;
-    for (int i = findl(begin); i <= findr(end); ++i) {
+    for (int i = findl(begin, Data); i <= findr(end, Data); ++i) {
         if (isPrime(Data[i])) {
             ++counter;
         }
@@ -62,7 +63,7 @@ bool isPrime(int val)
 // finds most left of the element val in Data
 // if not found returns Size + 1
 // binsearch
-int findl(int val)
+int findl(int val, const int* Data)
 {
     int beg = 0, end = Size;
     int mid = Size / 2;
@@ -85,7 +86,7 @@ int findl(int val)
 // finds most right of the element val in Data
 // if not found returns -1
 // binsearch
-int findr(int val)
+int findr(int val, const int* Data)
 {
     int beg = 0, end = Size;
     int mid = Size / 2;
