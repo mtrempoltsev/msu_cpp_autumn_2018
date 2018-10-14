e8c8adc287abc95c3bb8461cf2ffdcdfda2f3974
Konopatkin Oleg <37776122+Konopatkin-OV@users.noreply.github.com>
2018-10-14 23:19:27 +0300

    Add files via upload

--- Konopatkin_Oleg/hw_01/main.cpp
+++ Konopatkin_Oleg/hw_01/main.cpp
@@ -7,12 +7,12 @@ int is_prime(int n) {
     if (n < 2) {
         return 0;
     }
-	for (int i = 2; i * i <= n; ++i) {
-		if (n % i == 0) {
-			return 0;
-		}
-	}
-	return 1;
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 int main(int argc, char* argv[])
@@ -39,8 +39,8 @@ int main(int argc, char* argv[])
             while (cur + 1 < r && Data[cur] == Data[cur + 1]) {
                 ++cnt; ++cur;
             }
-        	sum += cnt * is_prime(Data[cur]);
-        	++cur;
+            sum += cnt * is_prime(Data[cur]);
+            ++cur;
         }
         std::cout << sum << std::endl;
     }
