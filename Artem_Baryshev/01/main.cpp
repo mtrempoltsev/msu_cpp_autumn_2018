2dc1257a9eec782ef7d28d2d63cc2ec11c4d7cac
artem <arteeemik@gmail.com>
2018-10-16 16:37:20 +0300

    HW1 - добавил явную проверку на выход за границы

--- Artem_Baryshev/01/main.cpp
+++ Artem_Baryshev/01/main.cpp
@@ -22,9 +22,9 @@ counting_prime_numbers(int * prime_numbers)
 }
 
 void
-counting_prefix_ans(int * prefix_ans, int * prime_numbers)
+counting_prefix_ans(int * prefix_ans, int size_prefix_ans, int * prime_numbers)
 {
-    for (int i = 0; i < Size; ++i) {
+    for (int i = 0; i < size_prefix_ans; ++i) {
         if (i == 0 && !prime_numbers[Data[i]]) {
             prefix_ans[i] = 1;
         } else if (!prime_numbers[Data[i]]) {
@@ -36,10 +36,14 @@ counting_prefix_ans(int * prefix_ans, int * prime_numbers)
 }
 
 int
-counting_ans(int position_first, int position_second, int * prefix_ans, int first_value, int second_value)
+counting_ans(int position_first, int position_second, int * prefix_ans, int size_prefix_ans, int first_value, int second_value)
 {
+    if (position_first < 0 || position_first >= size_prefix_ans
+        || position_second < 0 || position_second >= size_prefix_ans) {
+        return 0;
+    }
     if (position_first <= position_second
-            && Data[position_first] == first_value && Data[position_second] == second_value) {
+        && Data[position_first] == first_value && Data[position_second] == second_value) {
         if (position_first > 0) {
             return prefix_ans[position_second] - prefix_ans[position_first - 1];
         } else {
@@ -58,15 +62,15 @@ int main(int argc, const char * argv[]) {
     int *prime_numbers = new int[MAX_N]();
     counting_prime_numbers(prime_numbers);
     
-    int *prefix_ans = new int[Size + 1]();
-    counting_prefix_ans(prefix_ans, prime_numbers);
+    int *prefix_ans = new int[Size]();
+    counting_prefix_ans(prefix_ans, Size, prime_numbers);
     
     for (int i = 1; i < argc; i += 2) {
         int first_value = std::atoi(argv[i]);
         int second_value = std::atoi(argv[i + 1]);
         int position_first = std::lower_bound(Data, Data + Size, first_value) - Data;
         int position_second = std::upper_bound(Data, Data + Size, second_value) - Data - 1;
-        int ans = counting_ans(position_first, position_second, prefix_ans, first_value, second_value);
+        int ans = counting_ans(position_first, position_second, prefix_ans, Size, first_value, second_value);
         printf("%d\n", ans);
     }
     delete[] prime_numbers;
