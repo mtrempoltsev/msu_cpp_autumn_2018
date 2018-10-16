bc0a70e8a744e272bc3a3b46fc4e625b61df2ee3
Sergey Protserov <sergey.protserov@gmail.com>
2018-10-16 13:57:19 +0300

    Fix constness

--- Protserov/hw01/main.cpp
+++ Protserov/hw01/main.cpp
@@ -7,25 +7,25 @@ bool isprime(int);
 int main(int argc, char* argv[])
 {
     int a = 0, b = 0, cnt = 0;
-    int* begin_pos = nullptr, *end_pos = nullptr;
-    int* Data_begin = const_cast<int*>(Data);
-    int* Data_end = const_cast<int*>(Data + Size + 1);
+    const int* begin_pos = nullptr, *end_pos = nullptr;
+    const int* Data_begin = Data;
+    const int* Data_end = Data + Size + 1;
     if ((argc == 1) || (argc % 2 != 1)) {
         return -1;
     }
-    for (unsigned i = 1; i < argc - 1; i += 2) {
+    for (int i = 1; i < argc - 1; i += 2) {
         a = std::atoi(argv[i]);
         b = std::atoi(argv[i + 1]);
         if (a <= 0 || b <= 0) {
             return -1;
         }
         cnt = 0;
-        begin_pos = const_cast<int*>(std::lower_bound(Data_begin, Data_end, a));
-        end_pos = const_cast<int*>(std::upper_bound(Data_begin, Data_end, b));
+        begin_pos = std::lower_bound(Data_begin, Data_end, a);
+        end_pos = std::upper_bound(Data_begin, Data_end, b);
         if ((end_pos == Data_end) || (begin_pos == Data_end)) {
             std::cout << 0 << std::endl;
         } else {
-            for (int* myptr = begin_pos; myptr < end_pos; ++myptr) {
+            for (const int* myptr = begin_pos; myptr < end_pos; ++myptr) {
                 if (isprime(*myptr)) {
                     ++cnt;
                 }
