4344e77a592255c964d6e7c499978430cd78a06a
vozmo <43647842+vozmo@users.noreply.github.com>
2018-10-17 05:06:44 +0300

    Домашечка из 3ей лекции

---
+++ Novikov/01/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+#include <set>
+#include "numbers.dat"
+
+using namespace std;
+
+bool prostoe(int chislo) {
+	if(chislo < 2) return false;
+	for (int i = 2; i <= sqrt(chislo); i++)
+		if(chislo % i == 0) return false;
+	return true;
+}
+
+int main(int argc, char* argv[]) {
+	if((argc == 1) || (argc % 2 == 0)) return -1;
+	multiset<int> chisla;
+	for(int i = 1; i < Size; i++) chisla.insert(Data[i]);
+	for(int n = 1; n <= argc - 1; n += 2) {
+		int kolvo_vseh = 0;
+		int nachalo = atoi(argv[n]), konec = atoi(argv[n + 1]);
+		if(chisla.count(nachalo) && chisla.count(konec)) {
+			for(int i = nachalo; i <= konec; i++) {
+				int kolvo = chisla.count(i);
+				if(kolvo && prostoe(i)) kolvo_vseh += kolvo;
+			}
+		}
+		cout << kolvo_vseh << '\n';
+	}
+    return 0;
+}
