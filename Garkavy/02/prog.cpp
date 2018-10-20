61f9d3df2aaaa752b817c4e8f1a65b12523c16b1
Ivan Garkavy <jimmycalling@gmail.com>
2018-10-20 21:53:02 +0300

    done hw 02

--- Garkavy/02/prog.cpp
+++ Garkavy/02/prog.cpp
@@ -1,164 +1,168 @@
 #include <iostream>
-#include <vector>
 #include <stack>
 #include <cstdint>
-
-void debug(int n)
-{
-    std::cout << "test" << n << std::endl;
-}
-//DEBUG
+#include <cstring>
 
 class Calc
 {
-public:
-    Calc(std::string input): input(input) {}
     struct Lex {
         int64_t num;
         char op;
         Lex(char op = '\0', int64_t num = 0): num(num), op(op) {}
     };
     Lex cur_lex = Lex();
-    std::string input;
-    std::stack<Lex> st;
+    const char* input;
     size_t input_pos = 0;
+    std::stack<Lex> st;
 
-    char getc()
-    {
-        if(input_pos < input.size()) {
-            return input[input_pos++];
-        } else {
-            return '\0';
-        }
-    }
+    char getc();
+    void ungetc();
+    void read_lex();
+    int64_t expr2();
+    int64_t expr1();
+    int64_t operand();
 
-    void ungetc()
-    {
-        input_pos--;
-    }
-
-    void read_lex()
-    {
-        char c;
-        do {
-            c = getc();
-        } while(c == ' ');
-        // std::cout << "c=" << c << std::endl; //DEBUG
-        if(isdigit(c)) {
-            std::string buf;
-            buf.push_back(c);
-            while(c = getc()) {
-                if(!isdigit(c)) {
-                    break;
-                }
-                buf.push_back(c);
-            }
-            // std::cout << "(2)c=" << c << std::endl; //DEBUG
-            if(c) {
-                ungetc();
-            }
-            int64_t num = std::stoi(buf);
-            cur_lex = Lex('n', num);
-            return;
-        } else if(c == '+' || c == '-'
-        || c == '*' || c == '/'
-        || c == '\0'
-        ) {
-            cur_lex = Lex(c);
-            return;
-        } else {
-            std::cout << "error" << std::endl;
-            cur_lex =  Lex();
-            return;
-        }
-    }
-
-    void parse()
-    {
-        read_lex();
-        expr2();
-    }
-
-    void expr2()
-    {
-        // expr2 := expr1 {[+-] expr1}
-        expr1();
-        // debug(4); //DEBUG
-        char op = cur_lex.op;
-        while(op == '+' || op == '-') {
-            // debug(5); //DEBUG
-            read_lex();
-            expr1();
-            st.push(Lex(op));
-            op = cur_lex.op;
-        }
-    }
-
-    void expr1()
-    {
-        // expr1 := oper {[*/] oper}
-        operand();
-        char op = cur_lex.op;
-        // std::cout << "saved op = " << op << std::endl; //DEBUG
-        // debug(2); //DEBUG
-        while(op == '*' || op == '/') {
-            // debug(3); //DEBUG
-            read_lex();
-            operand();
-            st.push(Lex(op));
-            op = cur_lex.op;
-        }
-    }
-
-    void operand()
-    {
-        // debug(1); //DEBUG
-        // std::cout << "in operand: cur_lex.op == " << cur_lex.op << std::endl; //DEBUG
-        if(cur_lex.op == '-') {
-            read_lex();
-            if(cur_lex.op = 'n') {
-                st.push(Lex('n', -cur_lex.num));
-            } else {
-                std::cout << "error" << std::endl;
-                return;
-            }
-        } else {
-            if(cur_lex.op == 'n') {
-                st.push(Lex('n', cur_lex.num));
-            } else {
-                std::cout << "error" << std::endl;
-                return;
-            }
-        }
-        read_lex();
-        // std::cout << "operand read lex:" << cur_lex.op << cur_lex.num << std::endl; //DEBUG
-    }
-
-    // int64_t calc_one()
-    // {
-    //     Lex lex = st.top();
-    //     st.pop();
-    //     if(lex.op = 'n') {
-    //         return lex.
-    //     }
-    //     int64_t op2 = calc_one();
-    //     int64_t op1
-    // }
+public:
+    Calc(const char* input): input(input) {}
+    int64_t calculate();
 };
 
-int main(int argc, char* argv[]) {
-    if(argc <= 1) {
-        return -1;
+char Calc::getc()
+{
+    if(input_pos < strlen(input)) {
+        return input[input_pos++];
+    } else {
+        return '\0';
     }
-    Calc calc = Calc(std::string(argv[1]));
-    // for(int i = 0; i < 20; ++i) {
-    //     calc.read_lex();
-    //     std::cout << calc.cur_lex.op << ' ' << calc.cur_lex.num << std::endl;
-    // }
+}
 
-    calc.parse();
-    while(!calc.st.empty()) {
-        std::cout << calc.st.top().op << calc.st.top().num << std::endl;
-        calc.st.pop();
+void Calc::ungetc()
+{
+    input_pos--;
+}
+
+void Calc::read_lex()
+{
+    char c;
+    do {
+        c = getc();
+    } while(c == ' ');
+    if(isdigit(c)) {
+        char buf[32] = {0};
+        buf[0] = c;
+        size_t i = 1;
+        while(c = getc()) {
+            if(!isdigit(c)) {
+                break;
+            }
+            buf[i++] = c;
+        }
+        if(c) {
+            ungetc();
+        }
+        int64_t num = atoi(buf);
+        cur_lex = Lex('n', num);
+        return;
+    } else if(c == '+' || c == '-'
+    || c == '*' || c == '/'
+    || c == '\0'
+    ) {
+        cur_lex = Lex(c);
+        return;
+    } else {
+        throw false;
     }
+}
+
+int64_t Calc::expr2()
+{
+    // expr2 := expr1 {[+-] expr1}
+    int64_t res = expr1();
+    int64_t opnd;
+    while(true) {
+        if(cur_lex.op == '+') {
+            read_lex();
+            opnd = expr1();
+            res += opnd;
+        } else if(cur_lex.op == '-') {
+            read_lex();
+            opnd = expr1();
+            res -= opnd;
+        } else {
+            break;
+        }
+    }
+    return res;
+}
+
+int64_t Calc::expr1()
+{
+    // expr1 := oper {[*/] oper}
+    int64_t res = operand();
+    int64_t opnd;
+    while(true) {
+        if(cur_lex.op == '*') {
+            read_lex();
+            opnd = operand();
+            res *= opnd;
+        } else if(cur_lex.op == '/') {
+            read_lex();
+            opnd = operand();
+            if(opnd == 0) {
+                throw false;
+            }
+            res /= opnd;
+        } else {
+            break;
+        }
+    }
+    return res;
+}
+
+int64_t Calc::operand()
+{
+    int64_t ret;
+    if(cur_lex.op == '-') {
+        read_lex();
+        if(cur_lex.op == 'n') {
+            ret = -cur_lex.num;
+        } else {
+            throw false;
+        }
+    } else {
+        if(cur_lex.op == 'n') {
+            ret = cur_lex.num;
+        } else {
+            throw false;
+        }
+    }
+    read_lex();
+    return ret;
+}
+
+int64_t Calc::calculate()
+{
+    read_lex();
+    return expr2();
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 2) {
+        std::cout << "error" << std::endl;
+        return 1;
+    }
+    Calc calc = Calc(argv[1]);
+    // do {
+    //     calc.read_lex();
+    //     std::cout << calc.cur_lex.op << calc.cur_lex.num << std::endl;
+    // } while(calc.cur_lex.op);
+    try {
+        int64_t res = calc.calculate();
+        std::cout << res << std::endl;
+    }
+    catch(bool) {
+        std::cout << "error" << std::endl;
+        return 1;
+    }
+    return 0;
 }
