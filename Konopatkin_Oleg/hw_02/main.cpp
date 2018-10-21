6baaccc8da0cc41ec6e2dfa9f7d5d9fec254847c
Konopatkin Oleg <37776122+Konopatkin-OV@users.noreply.github.com>
2018-10-21 20:28:25 +0300

    Add files via upload

--- Konopatkin_Oleg/hw_02/main.cpp
+++ Konopatkin_Oleg/hw_02/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <exception>
 
-enum LEX_TYPE {NUM, OP, EOL};
+enum LEX_TYPE {NUM, OP, EOL};   // EOF for End Of Line
 enum OP_TYPE {ADD, SUB, MUL, DIV};
 
 
@@ -11,10 +12,7 @@ struct Lex {
     int64_t d;    // d for data
 };
 
-void error() {
-    std::cout << "error" << std::endl;
-    std::exit(1);
-}
+class Error : public std::exception {};   // empty class for exceptions
 
 
 // S -> A
@@ -23,8 +21,10 @@ void error() {
 // N -> -N | +N | [numeric constant]
 class Expr {
 private:
-    char *s;
+    const char *s;
     Lex lex;
+    bool correct;
+    int64_t value;
 
     int64_t parse_state_A() {
         int64_t cur = parse_state_M();
@@ -51,7 +51,7 @@ private:
                     parse_lex();
                     int64_t divisor = parse_state_M();
                     if (divisor == 0) {
-                        error();
+                        throw Error();
                     }
                     cur /= divisor;
                 }
@@ -62,7 +62,7 @@ private:
 
     int64_t parse_state_N() {
         if (lex.t == EOL) {
-            error();
+            throw Error();
         }
         if (lex.t == NUM) {
             int64_t cur = lex.d;
@@ -77,7 +77,7 @@ private:
                 return -parse_state_N();
             }
         }
-        error();
+        throw Error();
     }
 
     void parse_lex() {
@@ -108,20 +108,30 @@ private:
                     lex.d = DIV;
                     break;
                 default:
-                    error();
+                    throw Error();
             }
             ++s;
         }
     }
 
 public:
-    Expr(char *_s) {
+    Expr(const char *_s) {
         s = _s;
+        try {
+            parse_lex();
+            value = parse_state_A();
+            correct = true;
+        } catch (Error) {
+            correct = false;
+        }
     }
 
-    int64_t eval() {
-        parse_lex();
-        return parse_state_A();
+    bool is_correct() {
+        return correct;
+    }
+
+    int64_t get_value() {
+        return value;
     }
 };
 
@@ -129,12 +139,18 @@ public:
 int main(int argc, char* argv[])
 {
     if (argc != 2) {
-        error();
+        std::cout << "error" << std::endl;
+        return 1;
     }
     
     Expr expr(argv[1]);
 
-    std::cout << expr.eval() << std::endl;
+    if (expr.is_correct()) {
+        std::cout << expr.get_value() << std::endl;
+    } else {
+        std::cout << "error" << std::endl;
+        return 1;
+    }
 
     return 0;
 }
