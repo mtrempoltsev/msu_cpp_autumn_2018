f981d07a4f620830556880fab1ca978d4bf54c56
DKosov <dkosov420@gmail.com>
2018-10-21 23:41:08 +0300

    cosmetic

--- Kosov/3/main.cpp
+++ Kosov/3/main.cpp
@@ -65,7 +65,7 @@ int main(int argc, char **argv){
     try{
         Calculator calc(argv[1]);
         std::cout << calc;
-    }catch(char const* str){
+    }catch(const char* str){
         std::cout << str;
         return 1;
     }
