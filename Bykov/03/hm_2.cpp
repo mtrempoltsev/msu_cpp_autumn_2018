70498841e06fa3ded82c060d6eff4d91d5c2eece
Dmitry <dima13051998@gmail.com>
2018-10-22 22:03:06 +0300

    .

---
+++ Bykov/03/hm_2.cpp
@@ -0,0 +1,273 @@
+#include<string.h>
+#include<iostream>
+enum types{
+    NUM, SUM, MIN, MUL, DIV, SPC
+};
+
+class ERROR{};
+
+class leksem{
+    types type;
+    int64_t value;
+    bool isSign(char* str);
+    size_t isNum(char* str);
+    int normalizeFirst();
+public:
+    leksem *next;
+    leksem(char *expr);
+    types gettype(){
+        return type;
+    }
+    int64_t getval(){
+        return value;
+    }
+    void print(){
+        leksem *l = this;
+        while(l != nullptr){
+             std::cout<<l->type<<' ';
+             if(l->type == NUM)
+                 std::cout<<l->value<<' ';
+             l = l->next;
+        }
+    }
+    void normalize();
+    ~leksem(){
+        leksem *l = this;
+        while(l != nullptr){
+            leksem *lnext = l->next;
+            free(l);
+            l = lnext;
+        }
+    }
+};
+
+leksem* deleteLex(leksem *l){
+    leksem *lnext = l->next;
+    free(l);
+    return lnext;
+}
+
+int leksem::normalizeFirst(){
+    leksem *l = this;
+    int sign = 1;
+    while((l->type == SPC) || (l->type == MIN)){
+        if(l->type == MIN)
+            sign *= -1;
+        if(l->next == nullptr){
+            this->type = SPC;
+            return -1;
+        }
+        l = l->next;
+    }
+    if(l->type == NUM){
+        this->type = NUM;
+        this->value = l->value * sign;
+        if(this != l){
+            leksem *l2 = this->next;
+            while(l2 != l){
+                l2 = deleteLex(l2);
+            }
+            this->next = l->next;
+            free(l);
+        }
+    }
+    else{
+        this->type = SPC;
+        return -1;
+    }
+    return 0;
+}
+
+void leksem::normalize(){
+    if(normalizeFirst() == -1)
+        return;
+    leksem *lastImpLex = this;
+    leksem *iterLex = this->next;
+    int sign = 1;
+    while(iterLex != nullptr)
+        if(iterLex->type == SPC)
+            iterLex = deleteLex(iterLex);
+        else
+            if(iterLex->type == MIN)
+                if(lastImpLex->type != NUM){
+                    sign *= -1;
+                    iterLex = deleteLex(iterLex);
+                }
+                else{
+                    lastImpLex->next = iterLex;
+                    lastImpLex = lastImpLex->next;
+                    iterLex = iterLex->next;
+                }
+             else{
+                 lastImpLex->next = iterLex;
+                 lastImpLex = lastImpLex->next;
+                 iterLex = iterLex->next;
+                 if(lastImpLex->type == NUM){
+                    lastImpLex->value *= sign;
+                    sign = 1;
+                 }
+             }
+    lastImpLex->next = nullptr;
+}
+
+bool leksem::isSign(char *str){
+    char c = *str;
+    switch(c){
+        case '+':
+            this->type = SUM;
+            return 1;
+        case '-':
+            this->type = MIN;
+            return 1;
+        case '*':
+            this->type = MUL;
+            return 1;
+        case '/':
+            this->type = DIV;
+            return 1;
+        case ' ':
+            this->type = SPC;
+            return 1;
+        default:
+            return 0;
+    }
+}
+
+size_t leksem::isNum(char* str){
+    if((*str > '9') || (*str < '0'))
+        return 0;
+    char *iter = str;
+    this->type = NUM;
+    int64_t res=0;
+    char digit;
+    while(!((*iter > '9') || (*iter < '0'))){
+        digit = *iter++ - '0';
+        res = res * 10 + digit;
+    }
+    this->value = res;
+    size_t n = iter - str;
+    return n;
+}
+leksem::leksem(char *expr){
+    this->next = nullptr;
+    this->value = 0;
+    char *oldexpr = expr;
+    int mv = 0;
+    if(*expr != '\0')
+          mv += isNum(expr);
+          if(mv == 0)
+              mv += isSign(expr);
+    ERROR e;
+    if(mv == 0)
+        throw e;
+    expr += mv;
+    if(*expr != '\0')
+        this->next = new leksem(expr);
+}
+
+class CalcTree{
+    CalcTree *left;
+    CalcTree *right;
+    types type;
+    int64_t value;
+public:
+    CalcTree(leksem *l);
+    int calc();
+    ~CalcTree(){
+        if(left != nullptr)
+            delete left;
+        if(right != nullptr)
+            delete right;
+        free(this);
+    }
+};
+
+int CalcTree::calc(){
+    switch(type){
+        case SUM:
+            return left->calc() + right->calc();
+        case MIN:
+            return left->calc() - right->calc();
+        case MUL:
+            return left->calc() * right->calc();
+        case DIV:{
+            int r = right->calc();
+            if(r == 0)
+                throw ERROR();
+            return left->calc() / r;
+        }
+        case NUM:
+            return value;
+    }
+}
+
+int getPrior(leksem *l){
+    switch(l->gettype()){
+        case SUM:
+        case MIN: return 3;
+        case MUL:
+        case DIV: return 2;
+        case NUM: return 1;
+        default:
+            return 0;
+    }
+}
+
+leksem* getMaxPriorLeksem(leksem *l){
+    if(l == nullptr)
+        return nullptr;
+    leksem *maxl = l;
+    int max = getPrior(maxl);
+    while(l != nullptr){
+        if(getPrior(l) >= max){
+            maxl = l;
+            max = getPrior(l);
+        }
+        l = l->next;
+    }
+    if(max==0)
+        return nullptr;
+    return maxl;
+}
+
+CalcTree::CalcTree(leksem *l){
+    left = nullptr;
+    right = nullptr;
+    leksem *leftLex = l;
+    leksem *rightLex = getMaxPriorLeksem(l);
+    ERROR e;
+    if(rightLex == nullptr)
+        throw e;
+    if(rightLex == leftLex){
+        value = l->getval();
+        type = l->gettype();
+        if(type != NUM)
+            throw e;
+        return;
+    }
+    leksem *l2 = l;
+    while(l2->next != rightLex)
+        l2 = l2->next;
+    l2->next = nullptr;
+    type = rightLex->gettype();
+    value = rightLex->getval();
+    left = new CalcTree(leftLex);
+    right = new CalcTree(rightLex->next);
+    l2->next = rightLex;
+}
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        std::cout<<"error";
+        return 1;
+    }
+    try{
+        leksem *l = new leksem(argv[1]);
+        l->normalize();
+        CalcTree *tree = new CalcTree(l);
+        std::cout<<tree->calc();
+    }
+    catch(ERROR e){
+        std::cout<<"error";
+        return 1;
+    }
+}
