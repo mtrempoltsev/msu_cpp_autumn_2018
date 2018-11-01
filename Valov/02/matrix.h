30d5829c0a1aaefb5df5a605b7f80ea92453d880
Artem <43587572+temav@users.noreply.github.com>
2018-11-01 16:53:20 +0300

    Update matrix.h

--- Valov/02/matrix.h
+++ Valov/02/matrix.h
@@ -1,7 +1,7 @@
 class Matrix
 {
 	int* Mat;
-    const int Size, Rows, Columns;
+	const int Size, Rows, Columns;
 	class Matrix_Row
 	{
 		int size_R;
@@ -17,54 +17,53 @@ class Matrix
 		}
 	};
 public:
-    Matrix(int str, int col) : Rows(str), Columns(col), Size(str * col)
-    {
+	Matrix(int str, int col) : Rows(str), Columns(col), Size(str * col)
+	{
 		Mat = new int [Size];
 	}
 	const Matrix_Row operator [] (int index_row) const
-    {
+	{
 		if(index_row >= Rows)
 			throw std::out_of_range("");
-        return Matrix_Row(Mat + index_row * Columns, Columns);
-    }
+		return Matrix_Row(Mat + index_row * Columns, Columns);
+	}
 	Matrix_Row operator [] (int index_row)
-    {
+	{
 		if(index_row >= Rows)
 			throw std::out_of_range("");
-        return Matrix_Row(Mat + index_row * Columns, Columns);
-    }
-	Matrix& operator *= (int Mul)
-    {
-        for (int i = 0; i < Size; ++i)
+	 	return Matrix_Row(Mat + index_row * Columns, Columns);
+	}
+        Matrix& operator *= (int Mul)
+        {
+	        for (int i = 0; i < Size; ++i)
 			Mat[i] *= Mul;
-        return *this;
-    }
-	bool operator == (const Matrix& other)
-    {
-		if ((Rows != other.getRows()) || (Columns != other.getColumns())) 
-            return false;
-        for (int i = 0; i < Size; ++i)
+		return *this;
+ 	}
+        bool operator == (const Matrix& other)
+        {
+	        if ((Rows != other.getRows()) || (Columns != other.getColumns())) 
+	        	return 0;
+		for (int i = 0; i < Size; ++i)
 		{
 			if (Mat[i] != other.Mat[i])
 				return 0;
 		}
-        return 1;
-    }
+		return 1;
+	}
 	bool operator != (const Matrix& other)
-    {
-        return !(*this == other);
-    }
-    
-    int getRows() const
-    {
+	{
+		return !(*this == other);
+	}
+	int getRows() const
+	{
 		return Rows;
-    }
+	}
 	int getColumns() const
-    {
+	{
 		return Columns;
-    }
+	}
 	~Matrix()
 	{
-        delete[] Mat;
-    }
+		delete[] Mat;
+	}
 };
