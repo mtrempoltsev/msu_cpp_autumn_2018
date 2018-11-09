c768925fd4280aee8dd8324693f530d36b503c95
juicyfruityo <pooploorus@gmail.com>
2018-11-09 20:40:17 +0300

    hw5

--- Ustinov/05/serializer.h
+++ Ustinov/05/serializer.h
@@ -10,6 +10,7 @@ enum class Error {
 class Serializer {
   std::ostream& out;
   static constexpr char Separator = ' ';
+
  public:
   explicit Serializer(std::ostream& out_)
       : out(out_) {}
@@ -25,60 +26,61 @@ class Serializer {
   }
 
  private:
-   template<class T, class... ArgsT>
-   Error process(T&& val, ArgsT&&... args) {
+  template<class T, class... ArgsT>
+  Error process(T&& val, ArgsT&&... args) {
       if (process(val) == Error::NoError)
           return process(std::forward<ArgsT>(args)...);
       else
           return Error::CorruptedArchive;
-   }
+  }
 
-   Error process(bool val) {
+  Error process(bool val) {
       if (val)
           out << "true" << Separator;
       else
           out << "false" << Separator;
       return Error::NoError;
-   }
+  }
 
-   Error process(uint64_t val) {
+  Error process(uint64_t val) {
         out << val << Separator;
         return Error::NoError;
-   }
+  }
 
-   template <class T>
-   Error process(T&& val) {
+  template <class T>
+  Error process(T&& val) {
       return Error::CorruptedArchive;
-   }
+  }
 };
 
 class Deserializer {
   std::istream& in;
   static constexpr char Separator = ' ';
+
  public:
-   explicit Deserializer(std::istream& in_)
+  explicit Deserializer(std::istream& in_)
         : in(in_) {}
 
-   template <class T>
-   Error load(T& obj) {
+  template <class T>
+  Error load(T& obj) {
       return obj.serialize(*this);
-   }
+  }
 
-   template <class... ArgsT>
-   Error operator()(ArgsT&... args) {
+  template <class... ArgsT>
+  Error operator()(ArgsT&... args) {
       return process(args...);
-   }
+  }
 
  private:
-   template<class T, class... ArgsT>
-   Error process(T& val, ArgsT&... args) {
+  template<class T, class... ArgsT>
+  Error process(T& val, ArgsT&... args) {
       if (process(val) == Error::NoError)
           return process(args...);
       else
           return Error::CorruptedArchive;
-   }
+  }
 
-   Error process(bool& val) {
+  Error process(bool& val) {
       std::string str;
       in >> str;
       if (str == "true")
@@ -88,9 +90,9 @@ class Deserializer {
       else
           return Error::CorruptedArchive;
       return Error::NoError;
-   }
+  }
 
-   Error process(uint64_t& val) {
+  Error process(uint64_t& val) {
       std::string str;
       in >> str;
       if (str.c_str()[0] == '-') {
@@ -102,10 +104,10 @@ class Deserializer {
           return Error::CorruptedArchive;
       }
       return Error::NoError;
-   }
+  }
 
-   template <class T>
-   Error process(T& val) {
+  template <class T>
+  Error process(T& val) {
       return Error::CorruptedArchive;
-   }
+  }
 };
