85fe8eba067b7bf9308eeede5de05cb898b89866
cntkkjxrf <cntkkjxrf@gmail.com>
2018-11-14 18:48:03 +0300

    Update serializer.h

--- Astafeva/05/serializer.h
+++ Astafeva/05/serializer.h
@@ -67,7 +67,7 @@ public:
 
     template <class... ArgsT>
     Error operator()(ArgsT&&... args) {
-        return process(args...);
+        return process(std::forward<ArgsT>(args)...);
     }
 
 private:
