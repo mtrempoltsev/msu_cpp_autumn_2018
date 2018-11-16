bb4b0fe8a2941efe98287587265e1f64a89cc332
Artem Danko <ariolwork@gmail.com>
2018-11-16 14:20:06 +0300

    Update format.h

--- Danko Artem/hw6/format.h
+++ Danko Artem/hw6/format.h
@@ -4,26 +4,26 @@
 #include <vector>
 #include <sstream>
 
-class getter{
+class getter {
 public:
     std::vector<std::string> a;
     template <class... ArgsT>
-    getter(ArgsT&&... args){
+    getter(ArgsT&&... args) {
         get(std::forward<ArgsT>(args)...);
     }
-    std::vector<std::string> get_buf(){
+    std::vector<std::string> get_buf() {
         return a;
     }
 private:
     template <class T, class... ArgsT>
-    void get(T&& a1, ArgsT&&... args){
+    void get(T&& a1, ArgsT&&... args) {
         get(a1);
         get(std::forward<ArgsT>(args)...);
     }
     template <class T>
-    void get(T&& a1){
+    void get(T&& a1) {
         std::stringstream ss;
-        ss<<a1;
+        ss << a1;
         a.push_back(ss.str());
     }
     void get(){}
@@ -31,26 +31,27 @@ private:
 
 
 template <class... ArgsT>
-std::string format(const char* a, ArgsT... args){
+std::string format(const char* a, ArgsT... args) {
     std::vector<std::string> sargs = getter(std::forward<ArgsT>(args)...).get_buf();
     std::stringstream ss;
-    for (auto i = 0; a[i]!='\0'; i++) {
-        if (a[i] == '}') throw std::runtime_error("where is my {?");
-        if (a[i] == '{'){
+    for (auto i = 0; a[i] != '\0'; i++) {
+        if (a[i] == '}')
+            throw std::runtime_error("where is my {?");
+        if (a[i] == '{') {
             int ans = 0;
-            while(a[++i] != '}'){
-                if(a[i]<='9' && a[i]>='0'){
-                    ans=ans*10+a[i]-'0';
+            while (a[++i] != '}') {
+                if (a[i] <= '9' && a[i] >= '0') {
+                    ans = ans * 10 + a[i] - '0';
                 } else {
                     throw std::runtime_error("where is my }?");
                 }
             }
             if (ans >=sargs.size())
                 throw std::runtime_error("what is the number?");
-            ss<<sargs[ans];
+            ss << sargs[ans];
         }
         else {
-            ss<<a[i];
+            ss << a[i];
         }
     }
     return ss.str();
