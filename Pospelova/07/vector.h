778030b7a75c997e10e60164140ef3849cd139a2
Alina Pospelova <pospelowa.alina@yandex.ru>
2018-11-28 20:26:56 +0300

    Update vector.h
    forward - > move

--- Pospelova/07/vector.h
+++ Pospelova/07/vector.h
@@ -153,7 +153,7 @@ public:
 	void push_back(rvalue_reference value) {
 		if (alloc_size == size_)
 			this->reserve(alloc_size + BUF_SIZE);
-		A.construct(data + size_, forward<value_type>(value));
+		A.construct(data + size_, move(value));
 		size_++;
 	}
 	void push_back(const_reference value) {
