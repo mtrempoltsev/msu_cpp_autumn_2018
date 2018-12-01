d089ea9b5fbeeed7358f200d0a3e17f7aa0ddb0a
Artem Danko <ariolwork@gmail.com>
2018-12-01 23:27:22 +0300

    Add files via upload

---
+++ Danko Artem/ping_pong.cpp.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+class ping_pong{
+private:
+    std::mutex m_ping;
+    std::mutex m_pong;
+    void ping(const int n) {
+        if (n != 0) {
+            m_ping.lock();
+            printf("%s\n", "ping");
+            m_pong.unlock();
+            ping(n - 1);
+        }
+    }
+
+    void pong(const int n) {
+        if (n != 0) {
+            m_pong.lock();
+            printf("%s\n", "pong");
+            m_ping.unlock();
+            pong(n - 1);
+        }
+    }
+public:
+    ping_pong(const int n = 1){
+        bool is_now_ping = true;
+        m_pong.lock();
+        std::thread t(&ping_pong::ping, this, n);
+        std::thread t1(&ping_pong::pong, this, n);
+        t.join();
+        t1.join();
+    }
+};
+
+int main(){
+    ping_pong a(10);
+    return 0;
+}
