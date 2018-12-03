cf72d50aa86958abee8db2d8da6f4f283530e1e2
arinat79 <38926643+arinat79@users.noreply.github.com>
2018-12-03 16:51:32 +0300

    Update main.cpp

--- Travnikova/08/main.cpp
+++ Travnikova/08/main.cpp
@@ -1,43 +1,31 @@
 #include <iostream>
 #include <thread>
-#include <mutex>
+#include <atomic>
 
 using namespace std;
 
 enum { NUMBER = 500000 };
 
-mutex m;
-int proc = 0;
+atomic <int> proc;
 
 void proc1()
 {
 	for (int j = 0; j < NUMBER; j++) {
-		while (true) {
-			m.lock();
-			if (proc == 0) {
-				proc = 1;
-				cout << "ping\n";
-				m.unlock();
-				break;
-			}
-			m.unlock();
-		}
+		if (proc == 0) {
+			cout << "ping" << endl;
+			proc = 1;
+		} else j--; 
 	}
 }
 
 void proc2()
 {
 	for (int j = 0; j < NUMBER; j++) {
-		while (true) {
-			m.lock();
-			if (proc == 1) {
-				proc = 0;
-				cout << "pong\n";
-				m.unlock();
-				break;
-			}
-			m.unlock();
+		if (proc == 1) {
+			cout << "pong" << endl;
+			proc = 0;
 		}
+		else j--;
 	}
 }
 
