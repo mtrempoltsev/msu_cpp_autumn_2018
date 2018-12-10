4796c9807d80748b40b2992171925edfeddc1c34
arinat79 <38926643+arinat79@users.noreply.github.com>
2018-12-10 12:47:03 +0300

    Update vector.h

--- Travnikova/vector.h
+++ Travnikova/vector.h
@@ -33,7 +33,7 @@ public:
 };
 
 template<class T>
-class Iterator : public std::iterator<std::random_access_iterator_tag, T>
+class Iterator : public std::iterator<std::forward_iterator_tag, T>
 {
 public:
 	using reference = T&;
