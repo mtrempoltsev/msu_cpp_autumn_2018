73a82ec79306a09bf31af3288b6ef3b06676086c
MariaMsu <i.knaz@yandex.ru>
2018-12-31 20:18:59 +0300

    happy new year

---
+++ Cherepnina/HW_7/HW_7/cmake-build-debug/CMakeFiles/HW_7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW_7.dir/main.cpp.o"
+  "HW_7.pdb"
+  "HW_7"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
