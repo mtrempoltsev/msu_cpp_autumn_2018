defe8d06ad862a7196dc0b12de3a74b4ae748f8a
poliik <43605977+poliik@users.noreply.github.com>
2019-01-24 20:53:41 +0300

    Update bigInt.h

--- Karpikova/02/HW4/bigInt.h
+++ Karpikova/02/HW4/bigInt.h
@@ -1,32 +1,38 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 class BigInt
 {
-    static constexpr size_t Size = 256;
-    int8_t data_[Size];
+    int maxSize = 50;
+    char* data_;
     int curSize;
     bool sign = 0;
 public:
     BigInt(long long a = 0)
     {
-	if (a < 0) {
-	     a = -a;
-	     sign = 1;
-	}
-	if (a == 0) {
-            data_[0] = 0;
-	}
-	int i = 0;
-	while (a)
-	{
-	    data_[i++] = a % 10;
-	    a /= 10;
-	}
-	curSize = !i ? 1 : i;
-	for (int i = curSize; i < Size; ++i) {
-	    data_[i] = 0;
-	}
+        data_ = new char[maxSize];
+        if (a < 0) {
+             a = -a;
+             sign = 1;
+        }
+        if (a == 0) {
+                data_[0] = 0;
+        }
+        int i = 0;
+        while (a)
+        {
+            if (i == maxSize) {
+                maxSize <<= 1;
+                data_ = static_cast<char*>(realloc(data_, maxSize));
+            }
+            data_[i++] = a % 10;
+            a /= 10;
+        }
+        curSize = !i ? 1 : i;
+        for (int i = curSize; i < maxSize; ++i) {
+            data_[i] = 0;
+        }
     }
 
     bool operator==(const BigInt& other) const
@@ -38,7 +44,7 @@ public:
             return false;
         }
 
-        for (size_t i = 0; i < Size; ++i)
+        for (int i = 0; i < maxSize; ++i)
             if (data_[i] != other.data_[i])
                 return false;
 
@@ -47,16 +53,16 @@ public:
 
     bool operator!=(const BigInt& other) const
     {
-	return !(*this == other);
+        return !(*this == other);
     }
 
     BigInt operator-() const
     {
-	BigInt tmp = *this;
-	if (*this != 0) {
-		tmp.sign = !sign;
-	}
-	return tmp;
+        BigInt tmp = *this;
+        if (*this != 0) {
+            tmp.sign = !sign;
+        }
+        return tmp;
     }
 
     BigInt operator+(const BigInt &other) const
@@ -66,59 +72,63 @@ public:
 	}
 	if (sign == other.sign) {
 	    if (curSize < other.curSize) {
-		return other + *this;
+            return other + *this;
 	    }
 	    BigInt res;
 	    res.sign = sign;
 	    int flag = 0;
+	    if (curSize > res.maxSize) {
+	        res.maxSize <<= 1;
+            res.data_ = static_cast<char*>(realloc(res.data_, res.maxSize));
+	    }
 	    for (int i = 0; i <= curSize; ++i) {
-               res.data_[i] = data_[i] + other.data_[i] + flag;
-               flag = res.data_[i] / 10;
-               res.data_[i] %= 10;
-            }
+            res.data_[i] = data_[i] + other.data_[i] + flag;
+            flag = res.data_[i] / 10;
+            res.data_[i] %= 10;
+        }
 	    res.curSize = curSize;
             if (res.data_[curSize])
-		++res.curSize;
+                ++res.curSize;
 	        return res;
 	    } else {
-		return *this - (-other);
+            return *this - (-other);
 	    }
     }
 
     BigInt operator-(const BigInt &other) const
     {
-	if (other == 0) {
-	    return *this;
-	}
-	if (sign == other.sign) {
-	    if (*this == other) {
-		return 0;
-	    }
-	    if (!sign && (*this < other)) {
-		return -(other - *this);
-	    }
-	    if (sign && (*this > other))
-                return -other + *this;
-	    BigInt res = 0;
-	    res.sign = sign;
-	    res.curSize = curSize;
-            int flag = 0;
-	    for (int i = 0; i < curSize; ++i) {
-            res.data_[i] = data_[i] - other.data_[i] + flag;
-            if (res.data_[i] < 0) {
-                flag = -1;
-                res.data_[i] += 10;
-            } else {
-               flag = 0;
+        if (other == 0) {
+            return *this;
+        }
+        if (sign == other.sign) {
+            if (*this == other) {
+                return 0;
             }
-	}
-	while (!res.data_[res.curSize - 1] && res.curSize != 1) {
-            --res.curSize;
-	}
-	return res;
-	} else {
-	    return *this + (-other);
-	}
+            if (!sign && (*this < other)) {
+                return -(other - *this);
+            }
+            if (sign && (*this > other))
+                return -other + *this;
+            BigInt res = 0;
+            res.sign = sign;
+            res.curSize = curSize;
+            int flag = 0;
+            for (int i = 0; i < curSize; ++i) {
+                res.data_[i] = data_[i] - other.data_[i] + flag;
+                if (res.data_[i] < 0) {
+                    flag = -1;
+                    res.data_[i] += 10;
+                } else {
+                   flag = 0;
+                }
+            }
+            while (!res.data_[res.curSize - 1] && res.curSize != 1) {
+                --res.curSize;
+            }
+            return res;
+        } else {
+            return *this + (-other);
+        }
     }
 
     bool operator<(const BigInt& other) const
@@ -129,18 +139,18 @@ public:
         if (sign != other.sign) {
             return (sign > other.sign);
         }
-	if (curSize > other.curSize) {
-	    return sign;
-	}
-	if (curSize < other.curSize) {
-	    return !sign;
-	}
+        if (curSize > other.curSize) {
+            return sign;
+        }
+        if (curSize < other.curSize) {
+            return !sign;
+        }
         for (size_t i = curSize; i >= 0; --i) {
-	    if (data_[i] < other.data_[i]) {
-		return !sign;
-	    }
-	    if (data_[i] > other.data_[i]) {
-		return sign;
+            if (data_[i] < other.data_[i]) {
+                return !sign;
+            }
+            if (data_[i] > other.data_[i]) {
+                return sign;
             }
         }
         return false;
@@ -148,20 +158,20 @@ public:
 
     bool operator>(const BigInt& other) const
     {
-	if (*this == other) {
-	    return false;
-	}
-	return (other < *this);
+        if (*this == other) {
+            return false;
+        }
+        return (other < *this);
     }
 
     bool operator>=(const BigInt& other) const
     {
-	return !(*this < other);
+        return !(*this < other);
     }
 
     bool operator<=(const BigInt& other) const
     {
-	return !(*this > other);
+        return !(*this > other);
     }
 
     friend ostream &operator<<(ostream&, const BigInt&);
@@ -173,7 +183,7 @@ ostream &operator<<(ostream& out, const BigInt& obj)
     if (obj.sign == 1)
     out << '-';
     for (int i = obj.curSize - 1; i >= 0; --i) {
-	out << obj.data_[i] - 0;
+        out << obj.data_[i] - 0;
     }
     return out;
 }
