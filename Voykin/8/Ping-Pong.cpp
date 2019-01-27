6a3312ce520c7632cde2b97cb4efc4f74a8c638d
VoykinRS <43643941+VoykinRS@users.noreply.github.com>
2019-01-27 13:32:11 +0300

    Create Ping-Pong.cpp

---
+++ Voykin/8/Ping-Pong.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+using namespace std;
+condition_variable condition_;
+mutex mutex_;
+bool flag = false;
+
+
+void print_ping(int ping_pong_number)
+{
+  unique_lock<mutex> lock(mutex_);
+	for (int i = 0; i < ping_pong_number; i++)
+	{
+		while (flag)
+    {
+      condition_.wait(lock);
+    }
+		cout << "ping" << '\n';
+		flag = !flag;
+		condition_.notify_one();
+	}
+}
+
+void print_pong(int ping_pong_number)
+{
+  unique_lock<mutex> lock(mutex_);
+	for (int i = 0; i < ping_pong_number; i++)
+	{
+		while (!flag)
+    {
+      condition_.wait(lock);
+    }
+		cout << "pong" << '\n';
+		flag = !flag;
+		condition_.notify_one();
+	}
+}
+
+int main()
+{
+	int ping_pong_number = 500000;
+	thread thread_1(print_ping, ping_pong_number);
+	thread thread_2(print_pong, ping_pong_number);
+	thread_1.join();
+	thread_2.join();
+	return 0;
+}
